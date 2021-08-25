4ef724c319ba5d1d916547cc0222e53622694c2f
einstein07 <mkhsin035@myuct.ac.za>
2021-08-25 15:29:51 +0200

    A color sensor: returns distance to nearest object as well as the whether the said object is a robot or resource - if it's a resource the estimated size of the resource in terms of number of robots required to push it is returned, otherwise if not a resource 0 is returned.

---
+++ robogen/src/model/sensors/ColorSensor.cpp
@@ -0,0 +1,182 @@
+/*
+ * @(#) ColorSensor.cpp   1.0   Aug 19, 2021
+ *
+ * Sindiso Mkhatshwa (mkhsin035@myuct.ac.za)
+ *
+ * Nitschke Laboratory, UCT
+ *
+ * @(#) $Id$
+ */
+
+#include "ColorSensor.h"
+
+#include <algorithm>
+#include <iostream>
+#include <cmath>
+
+namespace robogen {
+
+const float ColorSensor::SENSOR_RANGE = 0.255;
+
+ColorSensor::ColorSensor(dSpaceID odeSpace,
+		std::vector<boost::shared_ptr<SimpleBody> > sensorBodies,
+		std::string baseLabel): odeSpace_(odeSpace), lastReadOutput_(0){
+	for(unsigned int i=0; i<sensorBodies.size(); i++){
+		for (dGeomID g=dBodyGetFirstGeom(sensorBodies[i]->getBody()); g
+				; g=dBodyGetNextGeom(g)){
+			sensorGeoms_.push_back(g);
+		}
+	}
+	raySpace_ = dHashSpaceCreate(0);
+
+	//Distance to detected object
+	sensors_.push_back(
+				boost::shared_ptr<IrSensorElement>(new IrSensorElement(baseLabel,
+						IrSensorElement::IR)));
+	//Sense if object is a robot
+	sensors_.push_back(
+			boost::shared_ptr<ColorSensorElement>(new ColorSensorElement(baseLabel,
+					ColorSensorElement::ROBOT)));
+	//Sense if object is a resource
+	sensors_.push_back(
+			boost::shared_ptr<ColorSensorElement>(new ColorSensorElement(baseLabel,
+					ColorSensorElement::RESOURCE)));
+	//Sense size of resource
+	sensors_.push_back(
+			boost::shared_ptr<ColorSensorElement>(new ColorSensorElement(baseLabel,
+					ColorSensorElement::RESOURCESIZE)));
+}
+
+ColorSensor::~ColorSensor() {
+	dSpaceDestroy(raySpace_);
+}
+
+
+
+struct RayTrace {
+	std::vector<dGeomID> ignoreGeoms;
+	bool isColliding;
+	osg::Vec3 collisionPoint;
+	ObjectData *objData;
+};
+
+void ColorSensor::collisionCallback(void *data, dGeomID o1, dGeomID o2){
+	RayTrace *r = (RayTrace*) data;
+	// ignore what needs ignoring
+	if (std::find(r->ignoreGeoms.begin(),r->ignoreGeoms.end(),
+			o1) != r->ignoreGeoms.end() ||
+			std::find(r->ignoreGeoms.begin(),r->ignoreGeoms.end(),
+					o2) != r->ignoreGeoms.end()){
+		return;
+	}
+	// if colliding get collision point
+	dContact contact;
+
+	if (dCollide(o1,o2,1,&contact.geom,sizeof(contact))) {
+		r->collisionPoint =  osg::Vec3(contact.geom.pos[0],
+				contact.geom.pos[1],
+				contact.geom.pos[2]);
+		r->isColliding = true;
+		std::cout << "Getting pointer to geom data" << std::endl;
+		r->objData = (ObjectData*)dGeomGetData(o1);
+		if(r->objData == NULL){
+			std::cout << "**Geom o1** is not either one of the objects of interest." << std::endl;
+			std::cout << "**Geom o2** is an object of interest." << std::endl;
+			r->objData = (ObjectData*)dGeomGetData(o2);
+		}
+		if(r->objData != NULL){
+			std::cout << "Managed to get a valid pointer." <<std::endl;
+		}
+	}
+}
+
+void ColorSensor::update(const osg::Vec3& position, const osg::Quat& attitude, boost::shared_ptr<Environment>& env) {
+	this->position_ = position;
+	this->attitude_ = attitude;
+
+	osg::Vec3 rayVector = attitude_ *  osg::Vec3(1,0,0);
+
+	// create ray
+	dGeomID ray = dCreateRay(raySpace_, SENSOR_RANGE);
+	// position ray
+	dGeomRaySet(ray, position_.x(), position_.y(), position_.z(),
+			rayVector.x(), rayVector.y(), rayVector.z());
+
+	// prepare collision data structure
+	RayTrace data;
+
+	// ignore sensor bodies
+	data.ignoreGeoms.insert(data.ignoreGeoms.end(),
+					sensorGeoms_.begin(), sensorGeoms_.end());
+
+	data.isColliding = false;
+
+	// perform collision
+	dSpaceCollide2((dGeomID)raySpace_, (dGeomID)odeSpace_, (void*)&data,
+					ColorSensor::collisionCallback);
+
+	float distance = SENSOR_RANGE;
+
+	// ray should be capped at SENSOR_RANGE, but just to make sure we don't
+	// allow bigger values here
+	if(data.isColliding &&
+			(data.collisionPoint - position_).length() < SENSOR_RANGE) {
+		distance = (data.collisionPoint - position_).length();
+
+		//std::cout << "pos: " << position_.x() << " " << position_.y() <<  " " << position_.z() << std::endl;
+		//std::cout << "ray: " << rayVector.x() << " " << rayVector.y() <<  " " << rayVector.z() << std::endl;
+		//std::cout << "collision: " << data.collisionPoint.x() << " " << data.collisionPoint.y() <<  " " << data.collisionPoint.z() << std::endl;
+
+	}
+
+	int value = -1;
+
+	// ray should be capped at SENSOR_RANGE, but just to make sure we don't
+	// allow bigger values here
+	if(data.isColliding && data.objData != NULL) {
+		value = (data.objData->isRobot)? ColorSensorElement::Type::ROBOT : ColorSensorElement::Type::RESOURCE;
+	}
+
+	dGeomDestroy(ray);
+	// want 0 when nothing is seen
+	sensors_[0]->updateValue( 1.0 - (distance / SENSOR_RANGE) );
+	//Either nothing was detected or the detected object is not a robot nor a resorce
+	if (value == -1){
+		sensors_[1]->updateValue(0.0);
+		sensors_[2]->updateValue(0.0);
+		sensors_[3]->updateValue(0.0);
+	}
+	// Resource detected
+	else if (value){
+		sensors_[1] -> updateValue(0.0);
+		sensors_[2] -> updateValue(1.0);
+
+		//--------------------------------------------------------
+		double size = 0.0;
+
+		boost::shared_ptr<BoxResource> resource = env->getResources()[data.objData->objectId];
+		int numRobotsAttached = resource->getNumberPushingRobots() + 1; //plus this robot
+		int numRobotsRequired = resource->getSize();
+		if (numRobotsAttached <= numRobotsRequired){
+			size = (double)(numRobotsAttached / numRobotsRequired);
+		}
+
+		//--------------------------------------------------------
+		sensors_[3] -> updateValue(size);
+		//boost::dynamic_pointer_cast<ColorSensorElement>(sensors_[3])->updateObjectId(data.objData->objectId);
+
+	}
+	//Robot detected
+	else{
+		sensors_[1] -> updateValue(1.0);
+		sensors_[2] -> updateValue(0.0);
+		sensors_[3] -> updateValue(0.0);
+	}
+
+}
+
+
+}
+
+
+
---
+++ robogen/src/model/sensors/ColorSensor.h
@@ -0,0 +1,129 @@
+/*
+ * @(#) ColorSensor.h   1.0   Aug 19, 2021
+ *
+ * Sindiso Mkhatshwa (mkhsin035@myuct.ac.za)
+ *
+ * Nitschke Laboratory, UCT
+ *
+ * @(#) $Id$
+ */
+#ifndef ROBOGEN_COLOR_SENSOR_H_
+#define ROBOGEN_COLOR_SENSOR_H_
+
+#include <boost/shared_ptr.hpp>
+#include <vector>
+#include <string>
+
+#include "Robogen.h"
+#include "model/sensors/Sensor.h"
+#include "model/SimpleBody.h"
+#include "SensorGroup.h"
+#include "IrSensor.h"
+#include "scenario/Environment.h"
+
+namespace robogen {
+
+//----------------------------------------------------------------------
+//----------------------------------------------------------------------
+class ColorSensorElement : public Sensor {
+public:
+
+	enum Type {
+		ROBOT,
+		RESOURCE,
+		RESOURCESIZE
+	};
+
+	inline ColorSensorElement(std::string baseLabel, Type type) :
+			Sensor(baseLabel + ((type == ROBOT) ?  "-Robot" :
+										"-Resource")),
+			baseLabel_(baseLabel), type_(type), objectId_(-1) {
+	}
+
+	inline Type getType() { return type_; }
+	inline const std::string &getBaseLabel() { return baseLabel_; }
+	inline int getObjectId() { return objectId_; }
+	inline void updateObjectId(int objectId) { objectId_ = objectId; }
+private:
+	std::string baseLabel_;
+	Type type_;
+	int objectId_;
+};
+//----------------------------------------------------------------------
+//----------------------------------------------------------------------
+class ColorSensor : public SensorGroup {
+
+public:
+
+	static const float SENSOR_RANGE;
+
+	/**
+	 * Initializes a color sensor
+	 */
+	ColorSensor(dSpaceID odeSpace,
+			std::vector<boost::shared_ptr<SimpleBody> > sensorBodies,
+			std::string baseLabel);
+
+	/**
+	 * Destructor
+	 */
+	virtual ~ColorSensor();
+
+	/**
+	 * Update the color sensor
+	 */
+	void update(const osg::Vec3& position, const osg::Quat& attitude, boost::shared_ptr<Environment>& env);
+
+	/**
+	 * Callback for collision handling between rays and the ODE space
+	 */
+	static void collisionCallback(void *data, dGeomID o1, dGeomID o2);
+
+private:
+	/**
+	 * Calculates intensity at light sensor from angle, light's intensity,
+	 * and distance. This is the function to be modified according to
+	 * light sensor calibration.
+	 * @param angle
+	 * @param lightIntensity
+	 * @param distance
+	 * @return intensity
+	 */
+	static double getIntensity(double angle, double lightIntensity,
+			double distance);
+
+	/**
+	 * Ode collision space
+	 */
+	dSpaceID odeSpace_;
+
+	/**
+	 * Position of the Color sensor
+	 */
+	osg::Vec3 position_;
+
+	/**
+	 * Attitude of the Color sensor
+	 */
+	osg::Quat attitude_;
+
+	/**
+	 * Space for rays
+	 */
+	dSpaceID raySpace_;
+
+	/**
+	 * Output of the last read
+	 */
+	float lastReadOutput_;
+
+	/**
+	 * ODE bodies of the Color sensor
+	 */
+	std::vector<dGeomID> sensorGeoms_;
+
+};
+
+}
+
+#endif /* ROBOGEN_COLOR_SENSOR_H_ */
