5aaf13ce27e6f94530ad266dc50e850cdb2fefcd
einstein07 <mkhsin035@myuct.ac.za>
2021-09-28 16:20:25 +0200

    Skeloton for a low res camera sensor, might well be replaced by the color sensor completely

---
+++ robogen/src/model/components/perceptive/LowResCameraSensorModel.cpp
@@ -0,0 +1,155 @@
+/*
+ * @(#) LowResCameraSensorModel.cpp   1.0   Aug 19, 2021
+ *
+ * Sindiso Mkhatshwa (mkhsin035@myuct.ac.za)
+ *
+ * Nitschke Laboratory, UCT
+ *
+ * @(#) $Id$
+ */
+
+#include "model/components/perceptive/LowResCameraSensorModel.h"
+
+
+namespace robogen {
+
+const float LowResCameraSensorModel::MASS = inGrams(2); //todo verify!
+const float LowResCameraSensorModel::SENSOR_BASE_WIDTH = inMm(34);
+const float LowResCameraSensorModel::SENSOR_BASE_THICKNESS = inMm(1.5);
+
+const float LowResCameraSensorModel::SENSOR_PLATFORM_WIDTH = inMm(23.3);
+const float LowResCameraSensorModel::SENSOR_PLATFORM_HEIGHT = inMm(23);
+const float LowResCameraSensorModel::SENSOR_PLATFORM_THICKNESS = inMm(5.5);
+
+const float LowResCameraSensorModel::SENSOR_DISPLACEMENT = inMm(6);
+
+LowResCameraSensorModel::LowResCameraSensorModel(dWorldID odeWorld, dSpaceID odeSpace,
+		std::string id) :
+		PerceptiveComponent(odeWorld, odeSpace, id) {
+}
+
+LowResCameraSensorModel::~LowResCameraSensorModel() {
+
+}
+
+bool LowResCameraSensorModel::initModel() {
+
+
+	//just dividing the mass by 2 for each component, is there a better way to do this?
+	sensorRoot_ = this->addBox(MASS/2.0, osg::Vec3(0, 0, 0),
+			SENSOR_BASE_THICKNESS, SENSOR_BASE_WIDTH, SENSOR_BASE_WIDTH,
+			B_SENSOR_BASE_ID);
+
+	boost::shared_ptr<SimpleBody> platform = this->addBox(MASS/2.0,
+			osg::Vec3(SENSOR_BASE_THICKNESS/2.0 +
+					  SENSOR_PLATFORM_THICKNESS/2.0, 0, 0),
+			SENSOR_PLATFORM_THICKNESS, SENSOR_PLATFORM_WIDTH,
+			SENSOR_PLATFORM_HEIGHT, B_SENSOR_PLATFORM_ID);
+
+	this->fixBodies(sensorRoot_, platform);
+
+	this->sensor_.reset(new LowResCameraSensor(this->getCollisionSpace(),
+			this->getBodies(), this->getId()));
+
+	return true;
+
+}
+
+boost::shared_ptr<SimpleBody> LowResCameraSensorModel::getRoot() {
+	return sensorRoot_;
+}
+
+boost::shared_ptr<SimpleBody> LowResCameraSensorModel::getSlot(unsigned int /*i*/) {
+	return sensorRoot_;
+}
+
+osg::Vec3 LowResCameraSensorModel::getSlotPosition(unsigned int i) {
+
+	if (i > 1) {
+		std::cout << "[LowResCameraSensorModel] Invalid slot: " << i << std::endl;
+		assert(i <= 1);
+	}
+
+	osg::Vec3 slotPos;
+	if (i == SLOT_A) {
+
+		osg::Vec3 curPos = this->sensorRoot_->getPosition();
+		osg::Vec3 slotAxis = this->getSlotAxis(i);
+		slotPos = curPos + slotAxis * (SENSOR_BASE_THICKNESS / 2);
+
+	}
+
+	return slotPos;
+
+}
+
+osg::Vec3 LowResCameraSensorModel::getSlotAxis(unsigned int i) {
+
+	if (i > 1) {
+		std::cout << "[LowResCameraSensorModel] Invalid slot: " << i << std::endl;
+		assert(i <= 1);
+	}
+
+	osg::Quat quat;
+	osg::Vec3 axis;
+
+	if (i == SLOT_A) {
+
+		quat = this->sensorRoot_->getAttitude();
+		axis.set(-1, 0, 0);
+
+	}
+
+	return quat * axis;
+
+}
+
+osg::Vec3 LowResCameraSensorModel::getSlotOrientation(unsigned int i) {
+
+	if (i > 1) {
+		std::cout << "[IrSensorModel] Invalid slot: " << i << std::endl;
+		assert(i <= 1);
+	}
+
+	osg::Quat quat;
+	osg::Vec3 axis;
+
+	if (i == SLOT_A) {
+
+		quat = this->sensorRoot_->getAttitude();
+		axis.set(0, 1, 0);
+
+	}
+	return quat * axis;
+
+}
+
+void LowResCameraSensorModel::getSensors(
+		std::vector<boost::shared_ptr<Sensor> >& sensors) {
+	sensors.resize(1);
+	sensors[0] = sensor_;
+}
+
+void LowResCameraSensorModel::updateSensors(boost::shared_ptr<Environment>& env) {
+
+	// Axis
+	osg::Quat quat = this->sensorRoot_->getAttitude();
+
+	osg::Vec3 curPos = this->sensorRoot_->getPosition();
+	osg::Vec3 axis(1, 0, 0);
+	osg::Vec3 sensorPos = curPos + quat * axis * SENSOR_DISPLACEMENT;
+	this->sensor_->update(sensorPos, this->sensorRoot_->getAttitude());
+	double value = 0.0;
+	if(sensor_->getObjectId() != -1){
+		boost::shared_ptr<BoxResource> resource = env->getResources()[sensor_->getObjectId()];
+		int numRobotsAttached = resource->getNumberPushingRobots() + 1; //plus this robot
+		int numRobotsRequired = resource->getSize();
+		if (numRobotsAttached <= numRobotsRequired){
+			value = (double)(numRobotsAttached / numRobotsRequired);
+		}
+
+	}
+	this->sensor_->updateValue(value);
+}
+
+}
---
+++ robogen/src/model/components/perceptive/LowResCameraSensorModel.h
@@ -0,0 +1,71 @@
+/*
+ * @(#) LowResCameraSensorModel.h   1.0   Aug 19, 2021
+ *
+ * Sindiso Mkhatshwa (mkhsin035@myuct.ac.za)
+ *
+ * Nitschke Laboratory, UCT
+ *
+ * @(#) $Id$
+ */
+
+#ifndef ROBOGEN_LOW_RES_CAMERA_SENSOR_MODEL_H_
+#define ROBOGEN_LOW_RES_CAMERA_SENSOR_MODEL_H_
+
+
+#include <boost/shared_ptr.hpp>
+#include "model/PerceptiveComponent.h"
+#include "model/sensors/LowResCameraSensor.h"
+
+namespace robogen {
+
+class LowResCameraSensorModel: public PerceptiveComponent {
+
+public:
+
+	static const float MASS;
+	static const float SENSOR_BASE_WIDTH;
+	static const float SENSOR_BASE_THICKNESS;
+	static const float SENSOR_PLATFORM_WIDTH;
+	static const float SENSOR_PLATFORM_HEIGHT;
+	static const float SENSOR_PLATFORM_THICKNESS;
+
+	static const float SENSOR_DISPLACEMENT;
+
+	static const unsigned int SLOT_A = 0;
+
+	static const unsigned int B_SENSOR_BASE_ID = 0;
+	static const unsigned int B_SENSOR_PLATFORM_ID = 1;
+
+	LowResCameraSensorModel(dWorldID odeWorld, dSpaceID odeSpace, std::string id);
+
+	virtual ~LowResCameraSensorModel();
+
+	virtual bool initModel();
+
+	virtual boost::shared_ptr<SimpleBody> getRoot();
+
+	virtual boost::shared_ptr<SimpleBody> getSlot(unsigned int i);
+
+	virtual osg::Vec3 getSlotPosition(unsigned int i);
+
+	virtual osg::Vec3 getSlotOrientation(unsigned int i);
+
+	virtual osg::Vec3 getSlotAxis(unsigned int i);
+
+	virtual void getSensors(std::vector<boost::shared_ptr<Sensor> >& sensors);
+
+	virtual void updateSensors(boost::shared_ptr<Environment>& env);
+
+private:
+
+	boost::shared_ptr<SimpleBody> sensorRoot_;
+
+	boost::shared_ptr<LowResCameraSensor> sensor_;
+
+};
+
+}
+
+
+
+#endif /* ROBOGEN_LOW_RES_CAMERA_SENSOR_MODEL_H_ */
