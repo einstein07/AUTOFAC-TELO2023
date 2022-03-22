2e268bcdead5278239ef12f97ea25d2cd8960ab8
einstein07 <mkhsin035@myuct.ac.za>
2022-03-22 17:38:51 +0200

    Removed cooperation code, optimized code for small resource collection - i.e. creation of fixed joints between agents and resources.

--- robogen/src/EDQD/PickUpHeuristic.cpp
+++ robogen/src/EDQD/PickUpHeuristic.cpp
@@ -24,190 +24,155 @@ namespace robogen{
 
 	osg::Vec2d PickUpHeuristic::step(){
 
-		/**
-		osg::Vec3d slotAxis = robot_ -> getCoreComponent() -> getSlotAxis(2);
-		osg::Vec3d slotPosition = robot_ -> getCoreComponent() -> getSlotPosition(2);
-		osg::Vec3d slotOrientation = robot_ -> getCoreComponent() -> getSlotOrientation(2);
-
-		std::cout 	<< "Slot axis: ("
-					<< slotAxis.x() << ", "
-					<< slotAxis.y() << ", "
-					<< slotAxis.z() << ").\nSlot position: ("
-					<< slotPosition.x() << ", "
-					<< slotPosition.y() << ", "
-					<< slotPosition.z() << ").\nSlot orientation: ("
-					<< slotOrientation.x() << ", "
-					<< slotOrientation.y() << ", "
-					<< slotOrientation.z() << ")"
-					<< std::endl;*/
 
 		if (heuristicpp_ -> ACTIVE){
 			std::cout << "ACTIVE" << std::endl;
 			return heuristicpp_ -> step();
 		}
 
+
+		double minDistanceToObject = 1/*0.05*/;
+		int objectId = -1;
+		double size = 0.0;
+
 		/**
 		 * If this agent is already attached to a resource,
 		 * then navigate to the target area
 		 */
-		osg::Vec3d targetPos =
-						Heuristic::getLocalPoint(
-												osg::Vec3f(
-															targetAreaPosition_.x(),
-															targetAreaPosition_.y(),
-															0
-														)
-												);
-		double minDistanceToObject = 1/*0.05*/;
-		ColorSensorElement::Type colorType;
-		IrSensorElement::Type IRtype;
-		int objectId = -1;
-		double size = 0.0;
+
 		if (robot_->isBoundToResource()){
-			/**std::cout 	<< "Bound to resource. Getting resouce from environment. Resource id: "
-						<< robot_->getBoundResourceId()
-						<< std::endl;*/
-			boost::shared_ptr<BoxResource> resource = env->getResources()[robot_->getBoundResourceId()];
-			//std::cout << "Done" <<std::endl;
+
+			objectId = robot_->getBoundResourceId();
+			boost::shared_ptr<BoxResource> resource = env->getResources()[objectId];
+
 			for (unsigned int  i = 0; i < robot_->getSensors().size(); ++i){
 				if (boost::dynamic_pointer_cast<TargetAreaDetectorElement>(robot_->getSensors()[i])) {
-					/**colorType = boost::dynamic_pointer_cast< ColorSensorElement>(robot_->getSensors()[i])->
-																getType();*/
-					/**std::cout << "It is a target area detector element. Value: "
-							<< boost::dynamic_pointer_cast<TargetAreaDetectorElement>(robot_->getSensors()[i])->
-							read()
-							<< std::endl;*/
+
 					if ( boost::dynamic_pointer_cast<TargetAreaDetectorElement>(robot_->getSensors()[i])->
 											read()){
-						/**colorType = boost::dynamic_pointer_cast< ColorSensorElement>(robot_->getSensors()[i])->
-												getType();*/
-						//std::cout << "Sensor number: "<< i << "is of type: " << colorType << std::endl;
-						//if (colorType == ColorSensorElement::TARGETAREA){
-							//std::cout << "Target area detected." << std::endl;
+
 							osg::Vec3d area = osg::Vec3d(targetAreaPosition_.x(), targetAreaPosition_.y(), 0);
 							if (robot_->isBoundToResource() && abs((distance(robot_->getCoreComponent()->getRootPosition(), area)) < 0.5) ){
 
-								resource->setCollected(true);
-								/**std::cout 	<< "resource set to collected."
-											<< std::endl;*/
-								if (boost::dynamic_pointer_cast<EDQDRobot>(robot_)){
-									boost::dynamic_pointer_cast<EDQDRobot>(robot_)->incResourceCounter(resource->getSize());
-									/**std::cout 	<< "resource counter incremented."
-												<< std::endl;*/
-									env->getGatheringZone()->addResource(resource);
-									if (boost::dynamic_pointer_cast<EDQDRobot>(robot_)){
+								resource -> setCollected(true);
 
-										std::cout 	<< "Resource added to gathering zone. Time bound to resource: "
-													<< boost::dynamic_pointer_cast<EDQDRobot>(robot_) -> getTimeResourceBound()
-													<< std::endl;
-										boost::dynamic_pointer_cast<EDQDRobot>(robot_) -> resetTimeResourceBound();
-									}
+								if (boost::dynamic_pointer_cast<EDQDRobot>(robot_)){
+
+									boost::dynamic_pointer_cast<EDQDRobot>(robot_)->incResourceCounter( resource->getSize() );
+
+									env->getGatheringZone()->addResource( resource );
+
+
+									std::cout 	<< "Resource added to gathering zone. Time bound to resource: "
+												<< boost::dynamic_pointer_cast<EDQDRobot>(robot_) -> getTimeResourceBound()
+												<< std::endl;
+									boost::dynamic_pointer_cast<EDQDRobot>(robot_) -> resetTimeResourceBound();
+
 
 								}
 							}
+							// Resource dropped, now wonder around looking for a new resource
 							return osg::Vec2d(-1000, -1000);
-						//}
+
 					}
 				}
 			}
-			if (resource -> getNumberPushingRobots() != resource -> getSize()){
+			return Heuristic::driveToTargetPosition(osg::Vec2d(/**targetPos.x()*/targetAreaPosition_.x(), targetAreaPosition_.y()/**targetPos.y()*/));
+			/**
+			 * Code commented out below only relevant when robots are cooperating
+			 */
+			/**if (resource -> getNumberPushingRobots() != resource -> getSize()){
 				/**std::cout 	<< "Robot - " << robot_ -> getId()
 							<< " waiting for help."
 							<< std::endl;*/
-				return osg::Vec2d(0, 0);
-			}
-			else{
-				return Heuristic::driveToTargetPosition(osg::Vec2d(/**targetPos.x()*/targetAreaPosition_.x(), targetAreaPosition_.y()/**targetPos.y()*/));
-			}
+				//return osg::Vec2d(0, 0);
+			/**}
+			else{*/
+			// Target area has not been detected yet, continue driving to target area
+			//}
 		}
+		/***
+		 * Not bound to any resource. Keep wandering to find resources that still need to be collected
+		 */
+		else{
 
-		for (unsigned int  i = 0; i < robot_->getSensors().size(); ++i){
-			double distance = 0;
+			/**
+			 * Check if there is any object detected by the IR sensors.
+			 * If there is, then check its distance to the robot, and then finally check
+			 * if the said object is a resource. If the said object is a resource then
+			 * obtain its object id and size
+			 */
 
-			if (boost::dynamic_pointer_cast< IrSensorElement>(robot_->getSensors()[i])) {
+			for (unsigned int  i = 0; i < robot_->getSensors().size(); ++i){
+				double distance = 0;
 
-				distance = boost::dynamic_pointer_cast< IrSensorElement>(robot_->getSensors()[i])->
-						read();
+				if (boost::dynamic_pointer_cast< IrSensorElement>(robot_->getSensors()[i])) {
 
-				if (distance < minDistanceToObject){
-					if (boost::dynamic_pointer_cast< ColorSensorElement>(robot_->getSensors()[i+2])) {
-						if ( boost::dynamic_pointer_cast< ColorSensorElement>(robot_->getSensors()[i+2])->
-								read()){
-							objectId = boost::dynamic_pointer_cast< ColorSensorElement>
-								(robot_->getSensors()[i+3])->getObjectId();
-							size = boost::dynamic_pointer_cast< ColorSensorElement>(robot_->getSensors()[i+3])->
-									read();
+					distance = boost::dynamic_pointer_cast< IrSensorElement>(robot_->getSensors()[i])->
+							read();
+
+					if (distance < minDistanceToObject){
+						if (boost::dynamic_pointer_cast< ColorSensorElement>(robot_->getSensors()[i+2])) {
+							if ( boost::dynamic_pointer_cast< ColorSensorElement>(robot_->getSensors()[i+2])->
+									read()){
+								objectId = boost::dynamic_pointer_cast< ColorSensorElement>
+									(robot_->getSensors()[i+3])->getObjectId();
+								size = boost::dynamic_pointer_cast< ColorSensorElement>(robot_->getSensors()[i+3])->
+										read();
+							}
 						}
+
 					}
-
-				}
-			}
-		}
-		//std::cout << "Drive to target area." << std::endl;
-		//return Heuristic::driveToTargetPosition(osg::Vec2d(/**targetPos.x()*/targetAreaPosition_.x(), targetAreaPosition_.y()/**targetPos.y()*/));
-
-		if (objectId == -1 || size == 0.0 ){
-			return osg::Vec2d(-1000, -1000);
-		}
-		else{ //Object detected and there is space for attachment
-		//	std::cout << "Object detected" << std::endl;
-			boost::shared_ptr<BoxResource> resource = env->getResources()[objectId];
-
-
-			// **************************
-
-			// Send control to motors
-			/*for (unsigned int i = 0; i < robot_->getMotors().size(); ++i) {
-
-				if (boost::dynamic_pointer_cast<RotationMotor>(robot_->getMotors()[i])) {
-					boost::dynamic_pointer_cast<RotationMotor>(robot_->getMotors()[i])
-							->setDesiredVelocity(
-													0.0,
-													0.1
-												);
-				}
-
-			}
-			for (unsigned int i = 0; i < robot_ -> getMotors().size(); ++i) {
-				robot_ -> getMotors()[i]->step(0.1 ) ; //* configuration->getActuationPeriod() );
-			}*/
-			// ***************************
-
-
-			if (resource->pickup(robot_)){
-				robot_ -> setBoundResourceId(objectId);
-				/**std::cout 	<< "Robot - "
-							<< robot_ -> getId()
-							<< " picked up resource" << std::endl;*/
-				if (resource -> getNumberPushingRobots() != resource -> getSize()){
-					return osg::Vec2d(0, 0);
-				}
-				else{
-					// Successfully attached to resource, now drive to target area to drop off resource
-					return Heuristic::driveToTargetPosition(osg::Vec2d(/**targetPos.x()*/targetAreaPosition_.x(), targetAreaPosition_.y()/**targetPos.y()*/));
 				}
 			}
 
-			else if (ENABLE_PICKUP_POSITIONING ){
-				std::cout << "resource could not be collected ";
-				if (resource -> isCollected()){
-					std::cout << "because it has been collected." <<std::endl;
-					return osg::Vec2d(-1000, -1000);
-
-				}
-				else{
-					heuristicpp_ -> setResource(resource);
-
-					return heuristicpp_ -> step();
-				}
-			}
-			else{
+			/**
+			 * If the detected resource has the maximum number of required robots attached to it - i.e. size == 0
+			 * or the obtained resource id is invalid then keep wandering the environment
+			 */
+			if ( objectId < 0 || size == 0.0 || objectId > env->getResources().size() ){
 				return osg::Vec2d(-1000, -1000);
 			}
+			/**
+			 * Resource detected and there is space for attachment
+			 */
+			else{
+				boost::shared_ptr<BoxResource> resource = env->getResources()[objectId];
+
+				if (resource->pickup(robot_)){
+					robot_ -> setBoundResourceId(objectId);
+					return Heuristic::driveToTargetPosition(osg::Vec2d(targetAreaPosition_.x(), targetAreaPosition_.y()));
+				}
+					/**
+					 * This code is only relevant for when robots are cooperating
+					 */
+					/**if (resource -> getNumberPushingRobots() != resource -> getSize()){
+						return osg::Vec2d(0, 0);
+					}
+					else{*/
+						// Successfully attached to resource, now drive to target area to drop off resource
+					//}
+
+
+				else if (ENABLE_PICKUP_POSITIONING ){
+					std::cout << "resource could not be collected ";
+					if (resource -> isCollected()){
+						std::cout << "because it has been collected." <<std::endl;
+						return osg::Vec2d(-1000, -1000);
+
+					}
+					else{
+						heuristicpp_ -> setResource(resource);
+
+						return heuristicpp_ -> step();
+					}
+				}
+				else{
+					return osg::Vec2d(-1000, -1000);
+				}
+			}
 		}
-
 	}
-
 }
 
 
