4f48cbff022d4f0265c1407b6d2b4cfb5462fb13
einstein07 <mkhsin035@myuct.ac.za>
2022-03-22 17:43:01 +0200

    Removed debug code - i.e. axis adding code on box resources

--- robogen/src/render/objects/BoxResourceRender.cpp
+++ robogen/src/render/objects/BoxResourceRender.cpp
@@ -19,21 +19,16 @@
 
 namespace robogen {
 
-BoxResourceRender::BoxResourceRender(boost::shared_ptr<BoxResource> resource): resource_(resource), debugActive_(false) {
+BoxResourceRender::BoxResourceRender(boost::shared_ptr<BoxResource> resource){
 
-	this->rootNode_ = osg::ref_ptr<osg::PositionAttitudeTransform>(
-				new osg::PositionAttitudeTransform());
+	/**this->rootNode_ = osg::ref_ptr<osg::PositionAttitudeTransform>(
+				new osg::PositionAttitudeTransform());*/
 
-}
+	rootNode_ = new osg::PositionAttitudeTransform();
 
-BoxResourceRender::~BoxResourceRender() {
-
-}
-
-bool BoxResourceRender::initRenderModel(){
 	osg::ref_ptr<osg::Box> box(
-			new osg::Box(osg::Vec3(0, 0, 0), fromOde(resource_->getGeomSize().x()),
-					fromOde(resource_->getGeomSize().y()), fromOde(resource_->getGeomSize().z())));
+			new osg::Box(osg::Vec3(0, 0, 0), fromOde(resource->getGeomSize().x()),
+					fromOde(resource->getGeomSize().y()), fromOde(resource->getGeomSize().z())));
 
 	osg::ref_ptr<osg::ShapeDrawable> boxDrawable(
 			new osg::ShapeDrawable(box.get()));
@@ -41,92 +36,17 @@ bool BoxResourceRender::initRenderModel(){
 	osg::ref_ptr<osg::Geode> geode(new osg::Geode());
 	geode->addDrawable(boxDrawable.get());
 	rootNode_->addChild(geode);
-	rootNode_->setUpdateCallback(new PositionObservableCallback(resource_));
-	if (isDebugActive()) {
-		this->showDebugView();
-	}
-	return true;
+	rootNode_->setUpdateCallback(new PositionObservableCallback(resource));
+
+
+}
+
+BoxResourceRender::~BoxResourceRender() {
 
 }
 
 osg::ref_ptr<osg::PositionAttitudeTransform> BoxResourceRender::getRootNode() {
 	return rootNode_;
 }
-
-bool BoxResourceRender::isDebugActive() {
-	return debugActive_;
-}
-
-void BoxResourceRender::setDebugActive(bool debugActive) {
-	this->debugActive_ = debugActive;
-}
-
-void BoxResourceRender::attachAxis(osg::Transform* transform) {
-
-	std::cout << "Displaying axis: x=red, y=green, z=blue" << std::endl;
-
-	osg::TessellationHints* hints = new osg::TessellationHints;
-
-	double height = 100;
-	double radius = .5;
-
-	osg::MatrixTransform* zmt = new osg::MatrixTransform();
-
-	transform->addChild(zmt);
-	osg::ShapeDrawable *zShape = new osg::ShapeDrawable(
-			new osg::Cylinder(osg::Vec3(0.0f, 0.0f, height / 2), radius,
-					height), hints);
-	osg::ShapeDrawable *zCone = new osg::ShapeDrawable(
-			new osg::Cone(osg::Vec3(0.0f, 0.0f, 1.0), radius + 1.0, 2.0),
-			hints);
-
-	osg::MatrixTransform* zmtCone = new osg::MatrixTransform();
-	osg::Geode *zgCone = new osg::Geode;
-
-	zmtCone->setMatrix(osg::Matrix::translate(0.0, 0.0, height));
-	transform->addChild(zmtCone);
-
-	zShape->setColor(osg::Vec4(0.0f, 0.0f, 1.0f, 1.0f));
-	zCone->setColor(osg::Vec4(0.0f, 0.0f, 1.0f, 1.0f));
-	osg::Geode *z = new osg::Geode;
-	z->addDrawable(zShape);
-	zgCone->addDrawable(zCone);
-	zmtCone->addChild(zgCone);
-	zmt->addChild(z);
-
-	osg::MatrixTransform* mt = new osg::MatrixTransform();
-	transform->addChild(mt);
-
-	osg::Matrix xMatrix = osg::Matrix::rotate(-osg::PI_2, 0.0, 1.0, 0.0);
-	mt->setMatrix(xMatrix);
-
-	osg::ShapeDrawable *xShape = new osg::ShapeDrawable(
-			new osg::Cylinder(osg::Vec3(0.0f, 0.0f, height / 2), radius,
-					height), hints);
-	xShape->setColor(osg::Vec4(1.0f, 0.0f, 0.0f, 1.0f));
-	osg::Geode *x = new osg::Geode;
-	x->addDrawable(xShape);
-	mt->addChild(x);
-
-	osg::MatrixTransform *yMt = new osg::MatrixTransform();
-	transform->addChild(yMt);
-	osg::Matrix yMatrix = osg::Matrix::rotate(osg::PI_2, 1.0, 0.0, 0.0);
-	yMt->setMatrix(yMatrix);
-
-	osg::ShapeDrawable *yShape = new osg::ShapeDrawable(
-			new osg::Cylinder(osg::Vec3(0.0f, 0.0f, height / 2), radius,
-					height), hints);
-	yShape->setColor(osg::Vec4(0.0f, 1.0f, 0.0f, 1.0f));
-	osg::Geode *y = new osg::Geode;
-	y->addDrawable(yShape);
-	yMt->addChild(y);
-}
-
-void BoxResourceRender::showDebugView() {
-
-		attachAxis(rootNode_);
-
-}
-
 
 }
