5dcc70840dda3477d28eda561b962777692044de
einstein07 <mkhsin035@myuct.ac.za>
2022-11-08 18:57:21 +0200

    Latest cluster custom path

--- robogen/src/cmake/CustomPath.cmake
+++ robogen/src/cmake/CustomPath.cmake
@@ -13,10 +13,37 @@ endif()
 
 if (UNIX)
 if ("${CMAKE_SYSTEM}" MATCHES "Linux")
-
+         set(UTILS_PATH "/mnt/lustre/groups/CSCI1142/RoboGen")
     # ODE
-	set (ODE_INCLUDE_PATH "/usr/local/include")
-	set (ODE_LIBRARIES "/usr/local/lib/libode.a")
+	set (ODE_INCLUDE_PATH "/mnt/lustre/groups/CSCI1142/RoboGen/include/ode")
+        set (ODE_LIBRARIES "/mnt/lustre/groups/CSCI1142/RoboGen/lib/libode.a")
+    # PNG
+	set (PNG_PNG_INCLUDE_DIR "/mnt/lustre/groups/CSCI1142/RoboGen/include/libpng16")
+        set (PNG_LIBRARY "/mnt/lustre/groups/CSCI1142/RoboGen/lib/libpng.a")
+
+    # OSG
+	set(ENV{OSG_DIR} "/mnt/lustre/groups/CSCI1142/RoboGen/bin" )
+        set(ENV{OSG_ROOT} "${UTILS_PATH}")
+    # Protobuf
+	set (Protobuf_INCLUDE_DIR "${UTILS_PATH}/include")
+        set (Protobuf_LIBRARY "${UTILS_PATH}/lib/libprotobuf.a")
+        set (PROTOBUF_PROTOC_EXECUTABLE "${UTILS_PATH}/bin/protoc")
+#-DProtobuf_LIBRARY=/mnt/lustre/groups/CSCI1142/RoboGen/lib/libprotobuf.so -DProtobuf_INCLUDE_DIR=/mnt/lustre/groups/CSCI1142/RoboGen/include
+    # BOOST
+	set (BOOST_ROOT "${UTILS_PATH}")
+        set (BOOST_LIB "${UTILS_PATH}/lib")
+        set (BOOST_LIB_DIR "${UTILS_PATH}/lib")
+        set (Boost_USE_STATIC_LIBS ON)
+        set (BOOST_IGNORE_SYSTEM_PATHS=1)
+    # JANSSON
+#	set (JANSSON_LIBRARIES="${UTILS_PATH}/lib/libjansson.a")
+#	set (JANSSON_INCLUDE_DIR="${UTILS_PATH}/include")
+
+#-DJANSSON_LIBRARIES=/mnt/lustre/groups/CSCI1142/RoboGen/lib/libjansson.so -DJANSSON_INCLUDE_DIR=/mnt/lustre/groups/CSCI1142/RoboGen/include/
+        set (JANSSON_LIBRARIES "${UTILS_PATH}/lib/libjansson.so")
+        set (JANSSON_INCLUDE_DIR "${UTILS_PATH}/include/")
+#        set (PNG_PNG_INCLUDE_DIR "/apps/chpc/bio/lib/png/1.6.21/include/libpng16")
+#        set (PNG_LIBRARY "/apps/chpc/bio/lib/png/1.6.21/lib/libpng.a")
 
 endif ()
 endif (UNIX)
