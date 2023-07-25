f7caf25d79641680d9951ddada6bc9d7441c40c3
einstein07 <mkhsin035@myuct.ac.za>
2023-07-25 11:02:45 +0200

    Added code to parse resource rotation axis and angle from config file

--- robogen/src/config/ResourcesConfig.h
+++ robogen/src/config/ResourcesConfig.h
@@ -35,9 +35,11 @@ public:
 	ResourcesConfig(const std::vector<osg::Vec3>& coordinates,
 			const std::vector<osg::Vec3>& sizes,
 			const std::vector<float> &densities,
+			const std::vector<osg::Vec3>& rotationAxes,
+			const std::vector<float> &rotationAngles,
 			const std::vector<int>& types) :
 			coordinates_(coordinates), sizes_(sizes), densities_(densities),
-			types_(types) {
+			rotationAxes_(rotationAxes), rotationAngles_(rotationAngles), types_(types) {
 
 	}
 
@@ -71,6 +73,21 @@ public:
 	const std::vector<float>& getDensities() const{
 		return densities_;
 	}
+	/**
+	 * Returns rotation axes.\n
+	 * @return the rotation axes
+	 */
+	const std::vector<osg::Vec3>& getRotationAxes() const{
+		return rotationAxes_;
+	}
+
+	/**
+	 * Returns the densities of obstacles.\n
+	 * @return the obstacle densities
+	 */
+	const std::vector<float>& getRotationAngles() const{
+		return rotationAngles_;
+	}
 
 	/**
 	 * Returns resource types.\n
@@ -87,13 +104,17 @@ public:
 		for (unsigned int i=0; i<coordinates_.size(); ++i){
 			robogenMessage::Resource *curr = message.add_resources();
 			curr->set_density(densities_[i]);
-                        curr->set_type(types_[i]);
+			curr->set_type(types_[i]);
 			curr->set_x(coordinates_[i].x());
 			curr->set_y(coordinates_[i].y());
 			curr->set_z(coordinates_[i].z());
 			curr->set_xsize(sizes_[i].x());
 			curr->set_ysize(sizes_[i].y());
 			curr->set_zsize(sizes_[i].z());
+			curr->set_xrotation(rotationAxes_[i].x());
+			curr->set_yrotation(rotationAxes_[i].y());
+			curr->set_zrotation(rotationAxes_[i].z());
+			curr->set_rotationangle(rotationAngles_[i]);
 		}
 	}
 
@@ -113,6 +134,16 @@ private:
 	 * Resource densities
 	 */
 	std::vector<float> densities_;
+
+	/**
+	 * Resource rotationAxes
+	 */
+	std::vector<osg::Vec3> rotationAxes_;
+
+	/**
+	 * Resource rotationAngles
+	 */
+	std::vector<float> rotationAngles_;
 
 	/**
 	 * resource-type: 1 - red, 2 - green,  3 - blue, 4 - yellow, 5 - brown
